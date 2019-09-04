853966d719f1ccb9f14bd7dd6380ee843b464433
Eriellen Souza <eriellenfabiane@hotmail.com>
2019-09-03 21:53:34 -0300

    Add project files.

---
+++ ByteBankImportacaoExportacao/Program.cs
@@ -0,0 +1,19 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankImportacaoExportacao 
+{ 
+    class Program 
+    { 
+        static void Main(string[] args)
+        { 
+            var conta = new ContaCorrente();
+            Console.ReadLine();
+        }
+    }    
+} 
+ 
