f07d3c66bf4181fa3cfa38744743a9351cd7d99e
Eriellen Souza <eriellenfabiane@hotmail.com>
2019-09-05 20:54:06 -0300

    Continuidade do curso de stream

---
+++ ByteBankImportacaoExportacao/1_LidandoComStreamDiretamente.cs
@@ -0,0 +1,68 @@
+﻿using ByteBankImportacaoExportacao.Modelos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankImportacaoExportacao
+{
+    partial class Program
+    {
+        static void LidandoComFileStreamDiretamente()
+        {
+            var caminhoArquivo = "contas.txt";
+
+            using (var fluxoDoArquivo = new FileStream(caminhoArquivo, FileMode.Open))
+            {
+                var buffer = new byte[1024];
+
+                var numeroDeBytesLidos = -1;
+
+                while (numeroDeBytesLidos != 0)
+                {
+                    numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                    EscreverBuffer(buffer, numeroDeBytesLidos);
+                }
+
+            }
+        }
+
+        static void EscreverBuffer(byte[] buffer, int bytesLidos)
+        {
+            var encoding = Encoding.Default;
+
+            var texto = encoding.GetString(buffer, 0, bytesLidos);
+
+            Console.Write(texto);
+
+        }
+
+        static ContaCorrente ConverterStringParaContaCorrente(string linha)
+        {
+            var campos = linha.Split(',');
+
+            var agencia = Convert.ToInt32(campos[0]);
+
+            var numero = Convert.ToInt32(campos[1]);
+
+            var saldo = Convert.ToDouble(campos[2]);
+
+            var nomeTitular = campos[3];
+
+            var titular = new Cliente();
+
+            titular.Nome = nomeTitular;
+
+            var resultado = new ContaCorrente(agencia, numero);
+
+            resultado.Titular = titular;
+
+            resultado.Depositar(saldo);
+
+            return resultado;
+        }
+
+    }
+}
--- ByteBankImportacaoExportacao/Program.cs
+++ ByteBankImportacaoExportacao/Program.cs
@@ -1,19 +1,33 @@
-﻿using ByteBankImportacaoExportacao.Modelos;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace ByteBankImportacaoExportacao 
 { 
-    class Program 
+    partial class Program 
     { 
         static void Main(string[] args)
-        { 
-            var conta = new ContaCorrente();
+        {
+            var caminhoArquivo = "contas.txt";
+
+            using (var fluxoDeArquivo = new FileStream(caminhoArquivo, FileMode.Open))
+            using (var leitor = new StreamReader(fluxoDeArquivo, Encoding.Default))
+            {
+                while (!leitor.EndOfStream)
+                {
+                    var linha = leitor.ReadLine();
+
+                    var contaCorrente = ConverterStringParaContaCorrente(linha);
+                    Console.WriteLine($"O número da conta: {contaCorrente.Numero}\n"+
+                                      $"Agência: {contaCorrente.Agencia}\n" +
+                                      $"Nome Tiular: {contaCorrente.Titular.Nome}\n" +
+                                      $"Saldo da conta: {contaCorrente.Saldo}\n\n\n");
+                    //Console.WriteLine(linha);
+                }
+            }           
+
             Console.ReadLine();
-        }
+        }       
     }    
 } 
  
