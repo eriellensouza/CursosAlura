a5d447f2dc6302402f0c184e96f27c340ac31e6a
eriellensouza <37486033+eriellensouza@users.noreply.github.com>
2019-11-26 15:31:23 -0300

    Add files via upload

---
+++ Lista/csharppt7-aula2/ByteBank.SistemaAgencia/ListaDeContaCorrente.cs
@@ -0,0 +1,122 @@
+﻿using ByteBank.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.SistemaAgencia
+{
+    // [item][item][item][item][item]
+    //                                ^
+    //                                 `- _proximaPosicao
+
+
+    public class ListaDeContaCorrente
+    {
+        private ContaCorrente[] _itens;
+        private int _proximaPosicao;
+        
+        public ListaDeContaCorrente(int capacidadeInicial = 5)
+        {
+            _itens = new ContaCorrente[capacidadeInicial];
+            _proximaPosicao = 0;
+        }
+
+        public void MeuMetodo(string texto = "texti padrao", int numero = 5)
+        {
+
+        }
+
+        public void Adicionar(ContaCorrente item)
+        {
+            VerificarCapacidade(_proximaPosicao + 1);
+
+            Console.WriteLine($"Adicionando item na posição {_proximaPosicao}");
+
+            _itens[_proximaPosicao] = item;
+            _proximaPosicao++;
+        }
+
+
+        private void VerificarCapacidade(int tamanhoNecessario)
+        {
+            if(_itens.Length >= tamanhoNecessario)
+            {
+                return;
+            }
+
+            int novoTamanho = _itens.Length * 2;
+
+            if (novoTamanho < tamanhoNecessario)
+            {
+                novoTamanho = tamanhoNecessario;
+            }
+
+            Console.WriteLine("Aumentando capacidade da lista!");
+
+            ContaCorrente[] novoArray = new ContaCorrente[novoTamanho];
+
+            for(int indice = 0; indice < _itens.Length; indice++)
+            {
+                novoArray[indice] = _itens[indice];
+                Console.WriteLine(".");
+            }
+
+            _itens = novoArray;
+        }
+
+        public void EscreverListaNaTela()
+        {
+            for (var i = 0; i < _proximaPosicao; i++)
+            {
+                ContaCorrente conta = _itens[i];
+                Console.WriteLine($"Numero da agencia e conta no indice {i}: {conta.Agencia} {conta.Numero}");
+            }
+
+        }
+
+        public void SomarNumeros(int[] numeros)
+        {
+            for (var i = 0; i < numeros.Length - 1; i += 2)
+            {
+                int primeiroNumero = numeros[i];
+                int segundoNumero = numeros[i + 1];
+
+                int soma = primeiroNumero + segundoNumero;
+
+                Console.WriteLine($"Primeiro numero: {primeiroNumero} Segundo Numero {segundoNumero} = {soma}");
+
+            }
+        }
+        public void Remover(ContaCorrente item)
+        {
+            int indiceItem = -1;
+            
+            for (int i = 0; i < _proximaPosicao-1; i++)
+            {
+                ContaCorrente itemAtual = _itens[i];
+
+                if (itemAtual.Equals(item))
+                {
+                    indiceItem = i;
+                    break;
+                }
+            }
+
+            //[0x01] [0x02] [0x03] [null]
+            //                              
+
+            for (int i = indiceItem; i < _proximaPosicao; i++)
+            {
+                _itens[i] = _itens[i + 1];
+            }
+
+            _proximaPosicao --;
+            _itens[_proximaPosicao] = null;
+        }
+
+
+
+    }
+}
---
+++ Lista/csharppt7-aula2/ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListaDeContaCorrente lista = new ListaDeContaCorrente();
+
+            ContaCorrente contaDoGui = new ContaCorrente(111, 11111111);
+
+
+            lista.Adicionar(contaDoGui);
+
+
+            lista.Adicionar(new ContaCorrente(874, 5679787));
+            lista.Adicionar(new ContaCorrente(874, 5679754));
+            lista.Adicionar(new ContaCorrente(874, 5679445));
+            lista.Adicionar(new ContaCorrente(874, 5679445));
+            lista.Adicionar(new ContaCorrente(874, 5679445));
+            lista.Adicionar(new ContaCorrente(874, 5679445));
+            lista.Adicionar(new ContaCorrente(874, 5679445));
+            lista.Adicionar(new ContaCorrente(874, 5679445));
+            lista.Adicionar(new ContaCorrente(874, 5679445));
+            lista.Adicionar(new ContaCorrente(874, 5679445));
+            lista.Adicionar(new ContaCorrente(874, 5679445));
+
+            lista.EscreverListaNaTela();
+
+            Console.WriteLine("Iniciando remoção!!");
+
+            lista.Remover(contaDoGui);
+
+            Console.WriteLine("Finalizando remoção!!");
+
+            lista.EscreverListaNaTela();
+
+            Console.ReadLine();
+        }
+
+
+
+        static void TestaArrayDeContaCorrente()
+        {
+            ContaCorrente[] contas = new ContaCorrente[]
+                {
+                    new ContaCorrente(874, 5679787),
+                    new ContaCorrente(874, 4456668),
+                    new ContaCorrente(874, 7781438)
+                };
+
+            for (int indice = 0; indice < contas.Length; indice++)
+            {
+                ContaCorrente contaAtual = contas[indice];
+                Console.WriteLine($"Conta {indice} {contaAtual.Numero}");
+            }
+        }
+
+        static void TestaArrayInt()
+        {
+            // ARRAY de inteiros, com 5 posições!
+            int[] idades = null;
+            idades = new int[3];
+
+            idades[0] = 15;
+            idades[1] = 28;
+            idades[2] = 35;
+
+            Console.WriteLine(idades.Length);
+
+            int acumulador = 0;
+            for (int indice = 0; indice < idades.Length; indice++)
+            {
+                int idade = idades[indice];
+
+                Console.WriteLine($"Acessando o array idades no índice {indice}");
+                Console.WriteLine($"Valor de idades[{indice}] = {idade}");
+
+                acumulador += idade;
+            }
+
+            int media = acumulador / idades.Length;
+            Console.WriteLine($"Média de idades = {media}");
+        }
+
+    }
+}
